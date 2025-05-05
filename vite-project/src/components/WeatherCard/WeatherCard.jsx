5157deb3f0a936a069ac7a67b6d1c881a056096b
Arielle <arielle.palmer12@gmail.com>
2025-05-05 15:25:49 -0500

    weather help

---
+++ vite-project/src/components/WeatherCard/WeatherCard.jsx
@@ -0,0 +1,17 @@
+import sunny from "../../assets/sunny.png";
+
+function WeatherCard() {
+  return (
+    <section className="weather-card">
+      <p className="weather-card__temp">75 &deg; F</p>
+      <img
+        src={sunny}
+        alt="sunny"
+        className="weather-card__image"
+        style={{ width: "100%", maxWidth: "870px", height: "auto" }}
+      />
+    </section>
+  );
+}
+
+export default WeatherCard;
